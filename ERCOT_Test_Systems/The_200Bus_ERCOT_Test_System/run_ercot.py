b4df0c227403d14b3ca38f499a005ec2abfaba0c
temcdrm <Thomas.McDermott@pnnl.gov>
2018-09-21 17:30:16 -0700

    200-bus model solves in DC mode

--- ERCOT_Test_Systems/The_200Bus_ERCOT_Test_System/run_ercot.py
+++ ERCOT_Test_Systems/The_200Bus_ERCOT_Test_System/run_ercot.py
@@ -2,8 +2,9 @@ import numpy as np
 import pypower.api as pp
 import tesp_support.api as tesp;
 
-ppc = tesp.load_json_case ('ppcase.json') # ('ercot_200.json')
-ppopt_regular = pp.ppoption(VERBOSE=0, OUT_ALL=1, PF_DC=0)
+# ppc = tesp.load_json_case ('ppcase.json')
+ppc = tesp.load_json_case ('ercot_200.json')
+ppopt_regular = pp.ppoption(VERBOSE=1, OUT_ALL=1, OUT_ALL_LIM=2, PF_DC=0, PF_ALG=1, OUT_RAW=1)
 rpf = pp.runpf (ppc, ppopt_regular)
 #print (rpf)
 #pp.printpf (rpf)
