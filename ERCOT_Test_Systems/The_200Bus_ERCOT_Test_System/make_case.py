94fe31e20e810f3b55b670df280e03ac3d6b52e8
temcdrm <Thomas.McDermott@pnnl.gov>
2018-09-22 18:09:25 -0700

    200-bus model runs

--- ERCOT_Test_Systems/The_200Bus_ERCOT_Test_System/make_case.py
+++ ERCOT_Test_Systems/The_200Bus_ERCOT_Test_System/make_case.py
@@ -8,8 +8,8 @@ import math
 # some of the system modeling assumptions 
 load_pf = 0.98
 load_qf = math.sqrt (1.0 - load_pf * load_pf)
-rxfpu = 0.01
-xxfpu = 0.10
+#rxfpu = 0.01
+#xxfpu = 0.10
 ehv_bus_offset = 3000
 dispatch = 49502.62 / 98869.50 
 
@@ -21,22 +21,27 @@ def find_bus(arr, n):
 
 def find_line(arr, n1, n2):
 	for ln in arr:
-		if int(ln[1]) == n1 and int(ln[2]) == n2:
-			return ln
+		if '//' not in ln[0]:
+			if int(ln[1]) == n1 and int(ln[2]) == n2:
+				return ln
 	return None
 
 if __name__ == '__main__':
-#	name,bus1,bus2,kV,length[miles],#parallel,r1[Ohms/mile],x1[Ohms/mile],b1[MVAR/mile],ampacity,capacity[MW]
+#   name,bus1,bus2,kV,length[miles],#parallel,r1[Ohms/mile],x1[Ohms/mile],b1[MVAR/mile],ampacity,capacity[MW]
 	dlines = np.genfromtxt('RetainedLines.csv', dtype=str, skip_header=1, delimiter=',') # ['U',int,int,float,float,int,float,float,float,float,float], skip_header=1, delimiter=',')
-#	bus,lon,lat,load,gen,diff
-	dbuses = np.genfromtxt('RetainedBuses.csv', dtype=[int, float, float, float, float, float], skip_header=1, delimiter=',')
+#   bus,lon,lat,load,gen,diff,caps
+	dbuses = np.genfromtxt('RetainedBuses.csv', dtype=[int, float, float, float, float, float, float], skip_header=1, delimiter=',')
 # idx,bus,mvabase,pmin,qmin,qmax,c2,c1,c0
 	dunits = np.genfromtxt('Units.csv', dtype=[int, int, float, float, float, float, float, float, float], skip_header=1, delimiter=',')
+# hvbus,mvaxf,rpu,xpu,tap
+	dxfmrs = np.genfromtxt('RetainedTransformers.csv', dtype=[int, float, float, float,float], skip_header=1, delimiter=',')
 
 	lbl345 = {}
 	lbl138 = {}
 	e345 = set()
 	e138 = set()
+	p345 = set()
+	p138 = set()
 	n345 = set()
 	n138 = set()
 	graph = nx.Graph()
@@ -44,6 +49,7 @@ if __name__ == '__main__':
 		if '//' not in e[0]:
 			n1 = int(e[1])
 			n2 = int(e[2])
+			npar = int(e[5])
 			graph.add_edge (n1, n2)
 			if float(e[3]) > 200.0:
 				n138.discard (n1)
@@ -52,11 +58,13 @@ if __name__ == '__main__':
 				n345.add (n2)
 				lbl345[(n1, n2)] = e[0]
 				e345.add ((n1, n2))
+				p345.add (npar)
 			else:
 				lbl138[(n1, n2)] = e[0]
 				n138.add (n1)
 				n138.add (n2)
 				e138.add ((n1, n2))
+				p138.add (npar)
 
 	print('There are', len(n138), 'HV buses and', len(e138), 'HV lines retained; ratio=', len(e138) / len(n138))
 	print('There are', len(n345), 'EHV buses and', len(e345), 'EHV lines retained; ratio=', len(e345) / len(n345))
@@ -95,13 +103,13 @@ if __name__ == '__main__':
 		Pd = float(ln[3])
 		Sd = Pd / load_pf
 		Qd = Sd * load_qf
-		Qd = 0
-		Sxf = math.ceil (Sd / 10.0) * 10.0
-		rxf = rxfpu * 100.0 / Sxf
-		xxf = xxfpu * 100.0 / Sxf
+		Qs = float(ln[6])
+#   	Sxf = math.ceil (Sd / 10.0) * 10.0
+#   	rxf = rxfpu * 100.0 / Sxf
+#   	xxf = xxfpu * 100.0 / Sxf
 		ppcase['bus'].append ([bus1, bustype, 0, 0, 0, 0, 1, 1, 0, 345, 1, 1.1, 0.9]) # no load on 345 kV
-		ppcase['bus'].append ([bus2, 1, Pd, Qd, 0, 0, 1, 1, 0, 138, 1, 1.1, 0.9])
-		ppcase['branch'].append ([bus1, bus2, rxf, xxf, 0.0, Sxf, Sxf, Sxf, 0.0, 0.0, 1, -360.0, 360.0]) # station transformer
+		ppcase['bus'].append ([bus2, 1, Pd, Qd, 0, Qs, 1, 1, 0, 138, 1, 1.1, 0.9])
+#   	ppcase['branch'].append ([bus1, bus2, rxf, xxf, 0.0, Sxf, Sxf, Sxf, 0.0, 0.0, 1, -360.0, 360.0]) # station transformer
 	for n in n138:
 		if n not in n345:
 			ln = find_bus (dbuses, n)
@@ -114,7 +122,8 @@ if __name__ == '__main__':
 			Pd = float(ln[3])
 			Sd = Pd / load_pf
 			Qd = Sd * load_qf
-			ppcase['bus'].append ([bus1, bustype, Pd, Qd, 0, 0, 1, 1, 0, 138, 1, 1.1, 0.9])
+			Qs = float(ln[6])
+			ppcase['bus'].append ([bus1, bustype, Pd, Qd, 0, Qs, 1, 1, 0, 138, 1, 1.1, 0.9])
 	Zbase = 345.0 * 345.0 / 100.0
 	for (n1, n2) in e345:
 		ln = find_line (dlines, n1, n2)
@@ -159,38 +168,52 @@ if __name__ == '__main__':
 		c0 = float(ln[8])
 		ppcase['gen'].append ([n1, Pg, 0.0, Qmax, Qmin, 1.0, Sg, 1, Sg, Pmin, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 		ppcase['gencost'].append ([2, 0, 0, 3, c2, c1, c0])
+	for ln in dxfmrs:
+		bus1 = int(ln[0])
+		bus2 = ehv_bus_offset + bus1
+		Sxf = float(ln[1])
+		rxf = float(ln[2]) * 100.0 / Sxf
+		xxf = float(ln[3]) * 100.0 / Sxf
+		tap = float(ln[4])
+		ppcase['branch'].append ([bus1, bus2, rxf, xxf, 0.0, Sxf, Sxf, Sxf, tap, 0.0, 1, -360.0, 360.0]) # station transformer
+
 	fp = open ('ercot_200.json', 'w')
 	json.dump (ppcase, fp, indent=2)
 	fp.close ()
 
 	print ('swing bus is', swing_bus)
 
-# draw the retained HV/EHV network
-#
-#	xy = {}
-#	lblbus345 = {}
-#	lblbus138 = {}
-#	for b in dbuses:
-#		xy[b[0]] = [b[1], b[2]]
-#		if b[0] in n345:
-#			lblbus345[b[0]] = str(b[0]) + ':' + str(int(b[5]))
-#		else:
-#			lblbus138[b[0]] = str(b[0]) + ':' + str(int(b[5]))
-#
-#	nx.draw_networkx_nodes (graph, xy, nodelist=list(n345), node_color='k', node_size=80, alpha=0.3)
-#	nx.draw_networkx_nodes (graph, xy, nodelist=list(n138), node_color='b', node_size=20, alpha=0.3)
-#	nx.draw_networkx_edges (graph, xy, edgelist=list(e345), edge_color='r', width=2, alpha=0.8)
-##	nx.draw_networkx_edges (graph, xy, edgelist=list(e345), edge_color='r', width=1, alpha=0.3)
-#	nx.draw_networkx_edges (graph, xy, edgelist=list(e138), edge_color='b', width=1, alpha=0.8)
-#	nx.draw_networkx_labels (graph, xy, lblbus345, font_size=8, font_color='k')
-#	nx.draw_networkx_labels (graph, xy, lblbus138, font_size=8, font_color='k')
-##	nx.draw_networkx_edge_labels (graph, xy, edge_labels=lbl345, label_pos=0.5, font_color='m', font_size=6)
-##	nx.draw_networkx_edge_labels (graph, xy, edge_labels=lbl138, label_pos=0.5, font_color='k', font_size=6)
-#
-#	plt.title ('Graph of Retained EHV and HV Lines')
-#	plt.xlabel ('Longitude [deg]')
-#	plt.ylabel ('Latitude [deg N]')
-#	plt.grid(linestyle='dotted')
-#	plt.show()
+ # draw the retained HV/EHV network
+
+	xy = {}
+	lblbus345 = {}
+	lblbus138 = {}
+	for b in dbuses:
+		xy[b[0]] = [b[1], b[2]]
+		if b[0] in n345:
+			lblbus345[b[0]] = str(b[0]) + ':' + str(int(b[5]))
+		else:
+			lblbus138[b[0]] = str(b[0]) + ':' + str(int(b[5]))
+
+	lst345 = list(e345)
+	lst138 = list(e138)
+	w345 = list(p345)
+	w138 = list(p138)
+
+	nx.draw_networkx_nodes (graph, xy, nodelist=list(n345), node_color='k', node_size=80, alpha=0.3)
+	nx.draw_networkx_nodes (graph, xy, nodelist=list(n138), node_color='b', node_size=20, alpha=0.3)
+	nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=w345, alpha=0.8)
+#   nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=1, alpha=0.3)
+	nx.draw_networkx_edges (graph, xy, edgelist=lst138, edge_color='b', width=w138, alpha=0.8)
+	nx.draw_networkx_labels (graph, xy, lblbus345, font_size=12, font_color='g')
+	nx.draw_networkx_labels (graph, xy, lblbus138, font_size=12, font_color='g')
+#   nx.draw_networkx_edge_labels (graph, xy, edge_labels=lbl345, label_pos=0.5, font_color='m', font_size=6)
+#   nx.draw_networkx_edge_labels (graph, xy, edge_labels=lbl138, label_pos=0.5, font_color='k', font_size=6)
+
+	plt.title ('Graph of Retained EHV and HV Lines')
+	plt.xlabel ('Longitude [deg]')
+	plt.ylabel ('Latitude [deg N]')
+	plt.grid(linestyle='dotted')
+	plt.show()
 
 
